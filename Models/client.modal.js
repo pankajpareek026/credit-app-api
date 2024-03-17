d99dadabe463bdbe7a8cd08615a7cd621b704cae
pankaj pareek <pankajpareek70168@gmail.com>
2024-03-17 12:16:10 +0530

    V2 Beta 0.2.0

--- Models/client.modal.js
+++ Models/client.modal.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose')
 const clientsSchema = new mongoose.Schema({
     parentId: {
         type: String,
-        require: true
+        required: true
     },
     name: {
         type: String,
         require: true,
         maxLength: [15, 'name must not be more than 15 charachers !']
     },
-    transactions: []
+}, {
+    timestamps: true,
 })
 
 module.exports = mongoose.model('clients', clientsSchema)
